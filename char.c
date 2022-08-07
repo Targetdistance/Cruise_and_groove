9a098b2672e1fc30fb94b56891abd4460333b18e
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2022-08-07 17:25:24 +0000

    test

---
+++ char.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print the value of a char
+ * @a: Char variable
+ * Return: 0
+ */
+
+int main(void)
+{
+	char a;
+	a = 'a';
+
+	printf("The value of %c is: %d", a, a);
+
+	return (0);
+}
